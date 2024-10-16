aeacbc7af9a31d490b6e4e2caf1f0195a2c2b31d
Leah Roberts <lrobe09@gmail.com>
2024-10-16 01:34:00 +0100

    update

--- general/parse_json.py
+++ general/parse_json.py
@@ -1,6 +1,13 @@
 import json
 import sys
 
+'''
+Run like this: 
+python parse_json.py json_file list_of_samples
+
+produces a file for each sample that lists the genes it hit
+'''
+
 gene_hits = sys.argv[1]
 samples = sys.argv[2]
 
@@ -8,7 +15,7 @@ y = open(gene_hits)
 data = json.load(y)
 
 with open(samples, "r") as fin:
-	for name in fin.readlines(): 
+	for name in samples.readlines(): 
 		name = name.rstrip()
 		outfilename = name + "_genes.txt"
 		with open(outfilename, "a") as fout:
@@ -16,6 +23,4 @@ with open(samples, "r") as fin:
 				fout.write("%s\n" % (i))
 		fout.close()
 	fin.close()	
-
-
 
